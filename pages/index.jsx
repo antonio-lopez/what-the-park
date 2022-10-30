3f143d2e2246b184d7bc8d8027f061e0b121e93f
Antonio Lopez <pezlop202@gmail.com>
2022-10-30 11:24:12 -0700

    Add try-catch api call

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,23 +1,31 @@
 import Card from '../components/Card';
 
-export default function Home({ data }) {
+const Home = ({ data }) => {
+  const parks = data;
+  console.log(parks);
   return (
     <section className='min-h-main bg-softWhite'>
       <div className='layout'>
         <div className='grid grid-cols-2 gap-3 py-7'>
-          {data.map((park) => (
+          {parks.map((park) => (
             <Card key={park.id} park={park} />
           ))}
         </div>
       </div>
     </section>
   );
-}
+};
 
-export async function getServerSideProps() {
-  const response = await fetch(
-    `https://developer.nps.gov/api/v1/parks?limit=5&api_key=${process.env.NEXT_PUBLIC_API_KEY}`
-  );
-  const data = await response.json();
-  return { props: data };
-}
+export const getServerSideProps = async () => {
+  try {
+    const response = await fetch(
+      `https://developer.nps.gov/api/v1/parks?limit=5&api_key=${process.env.NEXT_PUBLIC_API_KEY}`
+    );
+    const data = await response.json();
+    return { props: data };
+  } catch (err) {
+    return err;
+  }
+};
+
+export default Home;
