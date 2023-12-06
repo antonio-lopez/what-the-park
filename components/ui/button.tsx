0e1746cff4ff126bf6f568a46e09e2e0e543a77f
Antonio Lopez <36496887+antonio-lopez@users.noreply.github.com>
2023-12-05 17:20:20 -0800

    Update button size

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-12 px-6 py-2",
-        sm: "h-9 rounded-md px-3",
+        sm: "h-10 rounded-md px-5",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
       },
