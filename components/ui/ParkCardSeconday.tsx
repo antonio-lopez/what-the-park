a671bf39e8a13213d64f4788c40a971cbd7d6f3f
Antonio Lopez <36496887+antonio-lopez@users.noreply.github.com>
2023-12-07 09:15:43 -0800

    Add nextjs link to park page

--- components/ui/ParkCardSeconday.tsx
+++ components/ui/ParkCardSeconday.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Card } from "./card";
 import { Address, Images } from "@/lib/interface";
+import Link from "next/link";
 
 interface ParkProps {
   parkCode: String;
@@ -14,15 +15,17 @@ const ParkCardSeconday = ({ parkCode, addresses, images, name }: ParkProps) => {
 
   return (
     <Card className="h-[23.625rem] w-full rounded-md bg-white p-6 lg:h-[23.625rem] lg:w-[24.5625rem]">
-      <div className="relative mb-6 h-[15.625rem] w-full">
-        <Image
-          src={images[0]?.url}
-          alt={images[0]?.altText}
-          fill={true}
-          className="rounded-md object-cover"
-          sizes="(max-width: 900px) 33vw, 100vw"
-        />
-      </div>
+      <Link href={`/parks/${parkCode}`}>
+        <div className="relative mb-6 h-[15.625rem] w-full">
+          <Image
+            src={images[0]?.url}
+            alt={images[0]?.altText}
+            fill={true}
+            className="rounded-md object-cover"
+            sizes="(max-width: 900px) 33vw, 100vw"
+          />
+        </div>
+      </Link>
       <div>
         <p className="mb-2 text-lg font-semibold">{name}</p>
         <p className="text-sm font-medium text-slate-500">{location}</p>
