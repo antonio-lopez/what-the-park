fa11825aa99753de2ff4d50b4020708815c9654e
Antonio Lopez <36496887+antonio-lopez@users.noreply.github.com>
2023-12-13 18:41:30 -0800

    Add framer animations on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,37 @@ import Hero from "@/components/containers/Hero";
 import Highlights from "@/components/containers/Highlights";
 import Directions from "@/components/containers/Directions";
 import Footer from "@/components/ui/Footer";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Explore />
-      <Highlights />
-      <Directions />
+
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true, amount: 0.5 }}
+      >
+        <Explore />
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true, amount: 0.3 }}
+      >
+        <Highlights />
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true, amount: 0.5 }}
+      >
+        <Directions />
+      </motion.div>
+
       <Footer />
     </>
   );
